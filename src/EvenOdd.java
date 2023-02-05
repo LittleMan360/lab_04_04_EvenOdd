3eedc9cab9cd70268753d025fd66ade858a5a6a9
dalto <dalton.m.towe@gmail.com>
2023-02-05 03:57:39 -0500

    initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,15 @@
+public class EvenOdd
+{
+    public static void main(String[] args)
+    {
+
+        int numToExamine = 13;
+        int examinedNumber;
+
+        if (numToExamine % 2 == 0)
+            System.out.println(numToExamine + " is even");
+        else
+            System.out.println(numToExamine + " is odd");
+
+    }
+}
